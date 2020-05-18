55669c9bd47d0cf381cded0bb86e671f35e260e7
Maxim Nekrasov <kgtu@mail.ru>
2020-05-18 11:28:07 -0400

    first commit

---
+++ lesson2/3.js
@@ -0,0 +1,22 @@
+// Перепишите код, заменив оператор `switch` на оператор `if..else`:
+
+////////////////// Задание //////////////////
+// const value = 'c';
+
+// switch (value) {
+//     case 'a':
+//         console.log('a');
+//         break;
+
+//     case 'b':
+//     case 'c':
+//     case 'd':
+//     case 'e':
+//         console.log('others');
+//         break;
+
+//     default:
+//         console.log('unknown');
+// }
+
+////////////////// Решение //////////////////
