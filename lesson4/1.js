476e14066f0f426735ef5606ddd83ac255172868
Maxim Nekrasov <kgtu@mail.ru>
2020-05-25 16:25:40 -0400

    HW for lesson 4 added

--- lesson4/1.js
+++ lesson4/1.js
@@ -4,12 +4,19 @@
  * Создайте объект `person` c одним свойством `salary`.
  * При чтении этого свойства должна возвращаться строка с текстом.
  * Если до конца месяца осталось больше чем 20 дней — возвращается строка `good salary`, а если нет — `bad salary`
- * 
+ *
  * Условия:
  * - Свойство salary обязательно должно быть геттером.
  */
 
-const person = {};
+const person = {
+    get salary() {
+        let today = new Date();
+        let lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+        const oneDay = 1000 * 60 * 60 * 24;
+        return Math.ceil((lastDay.getTime() - today.getTime()) / (oneDay)) > 20 ? 'good salary' : 'bad salary';
+    }
+};
 
 // Решение
 
