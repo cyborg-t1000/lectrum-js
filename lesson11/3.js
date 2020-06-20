f8404f9b5847c292840a786360e4ddb20845bc92
Maxim Nekrasov <kgtu@mail.ru>
2020-06-20 13:45:39 -0400

    HW11 solution

---
+++ lesson11/3.js
@@ -0,0 +1,51 @@
+/**
+ * Задача 3.
+ *
+ * Улучшите функцию createFibonacciGenerator() из предыдущего примера.
+ *
+ * Теперь вызов функции createFibonacciGenerator() должен возвращать объект, который содержит два метода:
+ * - print — возвращает число из последовательности Фибоначчи;
+ * - reset — обнуляет последовательность и ничего не возвращает.
+ *
+ * Условия:
+ * - Задачу нужно решить с помощью замыкания.
+ */
+
+// Решение
+const createFibonacciGenerator = function () {
+    let numCurrent = 0;
+    let numNext = 1;
+
+    return {
+        print() {
+            const newNum = numCurrent + numNext;
+            numCurrent = numNext;
+            numNext = newNum;
+
+            return numCurrent;
+        },
+        reset() {
+            numCurrent = 0;
+            numNext = 1;
+        }
+    }
+}
+
+const generator1 = createFibonacciGenerator();
+
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 2
+console.log(generator1.print()); // 3
+console.log(generator1.reset()); // undefined
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 2
+
+const generator2 = createFibonacciGenerator();
+
+console.log(generator2.print()); // 1
+console.log(generator2.print()); // 1
+console.log(generator2.print()); // 2
+
+exports.createFibonacciGenerator = createFibonacciGenerator;
